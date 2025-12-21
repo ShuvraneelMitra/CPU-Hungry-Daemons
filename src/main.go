37293e6366e3269c42a51e0f19a631f37f161231
Shuvraneel Mitra <mitrashuvraneel@gmail.com>
2025-12-21 19:41:58 +0530

    Fixed Parser. God forbid any bugs in the future

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"time"
+	"fmt"
 
 	"github.com/ShuvraneelMitra/hungry-daemons/gui"
 	"github.com/ShuvraneelMitra/hungry-daemons/profiler"
@@ -11,7 +12,10 @@ func main(){
 	sm := profiler.NewGoRoutineSampler()
 	sm.SetSamplingFrequency(5)
 	stop := make(chan any)
-	time.AfterFunc(10 * time.Second, func(){close(stop)})
+	time.AfterFunc(10 * time.Second, func(){
+		close(stop)
+		fmt.Println("closed the stop channel")
+	})
 	channel := sm.Sample(stop)
 	profiler.Parse(stop, channel)
 
